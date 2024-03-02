3dc46730689a1b37d98c92cad142178636095752
S.A.T.D. Perara <136673697+thisara-dil@users.noreply.github.com>
2024-03-02 23:08:27 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+const isLeapYear = (year) => {
+    return (
+      (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) ||
+      (year % 100 === 0 && year % 400 === 0)
+    );
+  };
+  const getFebDays = (year) => {
+    return isLeapYear(year) ? 29 : 28;
+  };
+  let calendar = document.querySelector('.calendar');
+  const month_names = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
