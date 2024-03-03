1610bb4ab68168edc10929fc253bd72e8f875e08
S.A.T.D. Perara <136673697+thisara-dil@users.noreply.github.com>
2024-03-03 23:29:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,67 @@ const isLeapYear = (year) => {
     'November',
     'December',
   ];
+  let month_picker = document.querySelector('#month-picker');
+  const dayTextFormate = document.querySelector('.day-text-formate');
+  const timeFormate = document.querySelector('.time-formate');
+  const dateFormate = document.querySelector('.date-formate');
+  
+  month_picker.onclick = () => {
+    month_list.classList.remove('hideonce');
+    month_list.classList.remove('hide');
+    month_list.classList.add('show');
+    dayTextFormate.classList.remove('showtime');
+    dayTextFormate.classList.add('hidetime');
+    timeFormate.classList.remove('showtime');
+    timeFormate.classList.add('hideTime');
+    dateFormate.classList.remove('showtime');
+    dateFormate.classList.add('hideTime');
+  };
+  
+  const generateCalendar = (month, year) => {
+    let calendar_days = document.querySelector('.calendar-days');
+    calendar_days.innerHTML = '';
+    let calendar_header_year = document.querySelector('#year');
+    let days_of_month = [
+      31,
+      getFebDays(year),
+      31,
+      30,
+      31,
+      30,
+      31,
+      31,
+      30,
+      31,
+      30,
+      31,
+    ];
+    
+    let currentDate = new Date();
+    
+    month_picker.innerHTML = month_names[month];
+    
+    calendar_header_year.innerHTML = year;
+    
+    let first_day = new Date(year, month);
+  
+  
+  for (let i = 0; i <= days_of_month[month] + first_day.getDay() - 1; i++) {
+  
+      let day = document.createElement('div');
+  
+      if (i >= first_day.getDay()) {
+        day.innerHTML = i - first_day.getDay() + 1;
+
+        if (i - first_day.getDay() + 1 === currentDate.getDate() &&
+          year === currentDate.getFullYear() &&
+          month === currentDate.getMonth()
+        ) {
+          day.classList.add('current-date');
+        }
+      }
+      calendar_days.appendChild(day);
+    }
+  };
+  
+ 
